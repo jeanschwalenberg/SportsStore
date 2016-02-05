49fec624d43d7941586fc94c57f55c3f5dbdb7d4
jeanschwalenberg <jeanschwalenberg@gmail.com>
2016-02-05 14:39:01 -0700

    added constructor

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
 
-namespace SportsStore.WebUI.Controllers
-{
-    public class ProductController : Controller
-    {
-        // GET: Product
-        public ActionResult Index()
-        {
-            return View();
+namespace SportsStore.WebUI.Controllers {
+
+    public class ProductController : Controller {
+        private IProductRepository repository;
+
+    public ProductController(IProductRepository productRepository) {
+            this.repository = productRepository;
         }
     }
 }
