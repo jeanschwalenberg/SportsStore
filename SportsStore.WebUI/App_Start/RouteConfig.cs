8f62d5d6cd7fe31e9939f1547015cfded1be2778
jeanschwalenberg <jeanschwalenberg@gmail.com>
2016-02-27 13:50:13 -0700

    Changed composable URL via RouteConfig.cs
    to display only page numbers
    and not ? for query

--- SportsStore.WebUI/App_Start/RouteConfig.cs
+++ SportsStore.WebUI/App_Start/RouteConfig.cs
@@ -14,9 +14,9 @@ namespace SportsStore.WebUI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Product", action = "List", id = UrlParameter.Optional }
+                name: null,
+                url: "Page{page}",
+                defaults: new { Controller = "Product", action = "List" }
             );
         }
     }
