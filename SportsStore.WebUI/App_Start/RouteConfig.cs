ef4a978a001056a47e1ea1e5bc60d42611285144
jeanschwalenberg <jeanschwalenberg@gmail.com>
2016-02-27 13:53:48 -0700

    Re-added Default route to RouteConfig.cs
    was not supposed to replace Default route
    only supposed to add preferred route above it
    
    Oops.

--- SportsStore.WebUI/App_Start/RouteConfig.cs
+++ SportsStore.WebUI/App_Start/RouteConfig.cs
@@ -18,6 +18,15 @@ namespace SportsStore.WebUI
                 url: "Page{page}",
                 defaults: new { Controller = "Product", action = "List" }
             );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new {
+                    controller = "Product", action = "List",
+                    id = UrlParameter.Optional
+                }
+                );
         }
     }
 }
