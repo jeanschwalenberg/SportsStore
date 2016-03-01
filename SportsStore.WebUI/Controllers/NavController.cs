4e8c6aa45eaf221a8df500136cc2b0682ad85e60
jeanschwalenberg <jeanschwalenberg@gmail.com>
2016-03-01 15:06:44 -0700

    Changes to NavController.cs:
    added a constructor that accepts an IProductRepository implementation as its argument
    changed Menu() to include a LINQ query to pass a list of categories from the repository to the view

--- SportsStore.WebUI/Controllers/NavController.cs
+++ SportsStore.WebUI/Controllers/NavController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
 
-namespace SportsStore.WebUI.Controllers
-{
-    public class NavController : Controller
-    {
-        // GET: Nav
-        public string Menu() {
-            return "Hello from NavController";
+namespace SportsStore.WebUI.Controllers {
+
+    public class NavController : Controller {
+        private IProductRepository repository;
+
+        public NavController(IProductRepository repo) {
+            repository = repo;
+        }
+
+        public PartialViewResult Menu() {
+            IEnumerable<string> categories = repository.Products
+                                    .Select(x => x.Category)
+                                    .Distinct()
+                                    .OrderBy(x => x);
+            return PartialView(categories);
         }
     }
 }
