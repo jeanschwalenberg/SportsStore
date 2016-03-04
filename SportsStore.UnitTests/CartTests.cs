b64dc2756cb31e567a5640f13c86cdbbd28edee8
jeanschwalenberg <jeanschwalenberg@gmail.com>
2016-03-03 21:21:55 -0700

    added TestMethod Can_Clear_Contents
    12/12 tests pass

--- SportsStore.UnitTests/CartTests.cs
+++ SportsStore.UnitTests/CartTests.cs
@@ -94,6 +94,27 @@ namespace SportsStore.UnitTests {
             Assert.AreEqual(result, 450M);
         }
 
+        [TestMethod]
+        public void Can_Clear_Contents() {
+
+            //Arrange
+            Product p1 = new Product { ProductID = 1, Name = "P1", Price = 100M };
+            Product p2 = new Product { ProductID = 2, Name = "P2", Price = 50M };
+
+            //Arrange - create a new cart
+            Cart target = new Cart();
+
+            //Arrange - add some items
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+
+            //Act - reset the cart
+            target.Clear();
+
+            //Assert
+            Assert.AreEqual(target.Lines.Count(), 0);
+        }
+
 
     }
 }
