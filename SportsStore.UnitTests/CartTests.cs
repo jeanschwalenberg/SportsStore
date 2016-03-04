d53396b198e0f7b74399c0cc15352e6d728e0eb5
jeanschwalenberg <jeanschwalenberg@gmail.com>
2016-03-03 20:34:52 -0700

    added TestMethod Can_Remove_Line
    Russia takes the unit tests with a perfect [10]!!!

--- SportsStore.UnitTests/CartTests.cs
+++ SportsStore.UnitTests/CartTests.cs
@@ -49,5 +49,28 @@ namespace SportsStore.UnitTests {
             Assert.AreEqual(results[0].Quantity, 11);
             Assert.AreEqual(results[1].Quantity, 1);
         }
+
+        [TestMethod]
+        public void Can_Remove_Line() {
+            //Arrange - create some test products
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+            Product p3 = new Product { ProductID = 3, Name = "P3" };
+
+            //Arrange - create a new cart
+            Cart target = new Cart();
+            //Arrange - add some products to the cart
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 3);
+            target.AddItem(p3, 5);
+            target.AddItem(p2, 1);
+
+            //Act
+            target.RemoveLine(p2);
+
+            //Assert
+            Assert.AreEqual(target.Lines.Where(c => c.Product == p2).Count(), 0);
+            Assert.AreEqual(target.Lines.Count(), 2);
+        }
     }
 }
