9f260c3d84594ee371ab098edb6e376129060327
jeanschwalenberg <jeanschwalenberg@gmail.com>
2016-03-05 23:55:42 -0700

    added Adding_Product_To_Cart_Goes_To_Cart_Screen TestMethod
    to CartTests.cs
    
    all 14 tests passed THE FIRST TIME

--- SportsStore.UnitTests/CartTests.cs
+++ SportsStore.UnitTests/CartTests.cs
@@ -144,6 +144,27 @@ namespace SportsStore.UnitTests {
             Assert.AreEqual(cart.Lines.ToArray()[0].Product.ProductID, 1);
         }
 
+        [TestMethod]
+        public void Adding_Product_To_Cart_Goes_To_Cart_Screen() {
+            //Arrange - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product {ProductID = 1, Name = "P1", Category = "Apples"},
+            }.AsQueryable());
+
+            //Arrange -- create a Cart
+            Cart cart = new Cart();
+
+            //Arrange - create the controller
+            CartController target = new CartController(mock.Object);
+
+            //Act - add a product to the cart
+            RedirectToRouteResult result = target.AddToCart(cart, 2, "myUrl");
+
+            //Assert
+            Assert.AreEqual(result.RouteValues["action"], "Index");
+            Assert.AreEqual(result.RouteValues["returnUrl"], "myUrl");
+        }
 
     }
 }
