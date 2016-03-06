a6de9eace477c0d390aed01165478dbc6567fe3a
jeanschwalenberg <jeanschwalenberg@gmail.com>
2016-03-05 21:15:51 -0700

    added Index action to CartController.cs

--- SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -15,6 +15,13 @@ namespace SportsStore.WebUI.Controllers
             repository = repo;
         }
 
+        public ViewResult Index(string returnUrl) {
+            return View(new CartIndexViewModel {
+                Cart = GetCart().
+                ReturnUrl = returnUrl
+            });
+        }
+
         public RedirectToRouteResult AddToCart(int productId, string returnUrl) {
             Product product = repository.Products
                 .FirstOrDefault(p => p.ProductID == productId);
