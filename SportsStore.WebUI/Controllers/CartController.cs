f7a042e71be462a788466bbeafdddb65a6c21634
jeanschwalenberg <jeanschwalenberg@gmail.com>
2016-03-06 01:05:29 -0700

    squash! Summary.cshtml partial view for Cart

--- SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -44,9 +44,5 @@ namespace SportsStore.WebUI.Controllers
             }
             return RedirectToAction("Index", new { returnUrl });
         }
-
-        public PartialViewResult Summary(Cart cart) {
-            return PartialView(cart);
-        }
     }
 }
