af89a213dbeb5aafc077d7c6c33c48c27f9a7dcb
jeanschwalenberg <jeanschwalenberg@gmail.com>
2016-03-07 15:38:18 -0700

    NinjectDependencyResolver.cs now constructing
    new EmailSettings object
    when new instances of EmailOrderProcessor
    are created to service IOrderProcessor requests
    
    15 unit tests still passing...
      now in 1 second less. 6.
    
    neat

--- SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -8,6 +8,7 @@ using Moq;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Concrete;
 using SportsStore.Domain.Entities;
+using System.Configuration;
 
 namespace SportsStore.WebUI.Infrastructure {
 
@@ -29,8 +30,17 @@ namespace SportsStore.WebUI.Infrastructure {
 
         private void AddBindings() {
             kernel.Bind<IProductRepository>().To<EFProductRepository>();
+
+            EmailSettings emailSettings = new EmailSettings {
+                WriteAsFile = bool.Parse(ConfigurationManager
+                .AppSettings["Email.WriteAsFile"] ?? "false")
+            };
+
+            kernel.Bind<IOrderProcessor>().To<EmailOrderProcessor>()
+                .WithConstructorArgument("settings", emailSettings);
         }
 
+        
 
             /*Mock<IProductRepository> mock = new Mock<IProductRepository>();
             mock.Setup(m => m.Products).Returns(new List<Product> {
