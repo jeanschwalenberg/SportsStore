c9473665b60211ee8eb8b38685b75f2e8c1cdd32
jeanschwalenberg <jeanschwalenberg@gmail.com>
2016-03-15 17:07:54 -0600

    added FlexMenu.cshtml
    and necessary references to

--- SportsStore.WebUI/Controllers/NavController.cs
+++ SportsStore.WebUI/Controllers/NavController.cs
@@ -14,8 +14,7 @@ namespace SportsStore.WebUI.Controllers {
             repository = repo;
         }
 
-        public PartialViewResult Menu(string category = null,
-            bool horizontalLayout = false) { 
+        public PartialViewResult Menu(string category = null) { 
 
             ViewBag.SelectedCategory = category;
 
@@ -24,8 +23,7 @@ namespace SportsStore.WebUI.Controllers {
                                     .Distinct()
                                     .OrderBy(x => x);
 
-            string viewName = horizontalLayout ? "MenuHorizontal" : "Menu";
-            return PartialView(viewName, categories);
+            return PartialView("FlexMenu", categories);
         }
     }
 }
