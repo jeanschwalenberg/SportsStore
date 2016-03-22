5bf2f98c12268e51b882f5e34933c25fbff2545d
jeanschwalenberg <jeanschwalenberg@gmail.com>
2016-03-22 16:13:51 -0600

    added Can_Save_Valid_Changes
    and Cannot_Save_Invalid_Changes
    tests to AdminTests.cs
    
    and updated _AdminLayout.cshtml

--- SportsStore.UnitTests/AdminTests.cs
+++ SportsStore.UnitTests/AdminTests.cs
@@ -80,5 +80,45 @@ namespace SportsStore.UnitTests {
             //Assert
             Assert.IsNull(result);
         }
+
+        [TestMethod]
+        public void Can_Save_Valid_Changes() {
+
+            //Arrange - create mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            //Arrange - create the controller
+            AdminController target = new AdminController(mock.Object);
+            //Arrange - create a product
+            Product product = new Product { Name = "Test" };
+
+            //Act - try to save the product
+            ActionResult result = target.Edit(product);
+
+            //Assert - check that the repository was called
+            mock.Verify(m => m.SaveProduct(product));
+            //Assert - check the method result type
+            Assert.IsNotInstanceOfType(result, typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public void Cannot_Save_Invalid_Changes() {
+
+            //Arrange - create mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            //Arrange - create the controller
+            AdminController target = new AdminController(mock.Object);
+            //Arrange - create a product
+            Product product = new Product { Name = "Test" };
+            //Arrange - add an error too the model state
+            target.ModelState.AddModelError("error", "error");
+
+            //Act - try to save the product
+            ActionResult result = target.Edit(product);
+
+            //Assert - check that repository was not called
+            mock.Verify(m => m.SaveProduct(It.IsAny<Product>()), Times.Never());
+            //Assert - check the method result type
+            Assert.IsNotInstanceOfType(result, typeof(ViewResult));
+        }
     }
 }
