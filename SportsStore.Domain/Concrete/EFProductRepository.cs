b59160eb1facbf0231b7278d2c3112a0baf647a0
jeanschwalenberg <jeanschwalenberg@gmail.com>
2016-03-23 13:19:30 -0600

    added Delete post action
    with TempData message if Delete action called on correct product
    
    added Can_Delete_Valid_Products test
    
    added DeleteProduct action to EFProductRepository.cs
    
    added DeleteProduct parent action to IProductRepository.cs
    
    24 karat unit test gold

--- SportsStore.Domain/Concrete/EFProductRepository.cs
+++ SportsStore.Domain/Concrete/EFProductRepository.cs
@@ -29,7 +29,15 @@ namespace SportsStore.Domain.Concrete {
                     dbEntry.Category = product.Category;
                 }
             }
-            context.SaveChanges();
+        }
+
+            public Product DeleteProduct(int productId) {
+            Product dbEntry = context.Products.Find(productId);
+            if (dbEntry != null) {
+                context.Products.Remove(dbEntry);
+                context.SaveChanges();
+            }
+            return dbEntry;
         }
     }
 }
