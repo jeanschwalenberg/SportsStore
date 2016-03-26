2012996bec9e20a28b74aea457c42ab8d9431aa1
jeanschwalenberg <jeanschwalenberg@gmail.com>
2016-03-26 09:37:02 -0600

    + AdminSecurityTests.cs
    + SQLQuery1
    
    added ImageMimeType and ImageData properties to EFProductRepository.cs
    added ImageMimeType and ImageData properties to Product.cs
    added HttpPostedFileBase image as overload parameter to post action Edit
    
    changed some stuff in Edit.cshtml - should have committed last night

--- SportsStore.WebUI/Controllers/AdminController.cs
+++ SportsStore.WebUI/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Mvc;
+using System.Web;
 using System.Linq;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
@@ -24,8 +25,13 @@ namespace SportsStore.WebUI.Controllers {
         }
 
         [HttpPost]
-        public ActionResult Edit(Product product) {
+        public ActionResult Edit(Product product, HttpPostedFileBase image = null) {
             if (ModelState.IsValid) {
+                if (image != null) {
+                    product.ImageMimeType = image.ContentType;
+                    product.ImageData = new byte[image.ContentLength];
+                    image.InputStream.Read(product.ImageData, 0, image.ContentLength);
+                }
                 repository.SaveProduct(product);
                 TempData["message"] = string.Format("{0} has been saved", product.Name);
                 return RedirectToAction("Index");
