5d7028d369bb11ef26875e07e5763f2ff945eaf1
jeanschwalenberg <jeanschwalenberg@gmail.com>
2016-03-26 15:03:34 -0600

    *mic drop*

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -36,5 +36,15 @@ namespace SportsStore.WebUI.Controllers {
             };
             return View(model);
         }
+
+        public FileContentResult GetImage(int productId) {
+            Product prod = repository.Products
+                .FirstOrDefault(p => p.ProductID == productId);
+            if (prod != null) {
+                return File(prod.ImageData, prod.ImageMimeType);
+            } else {
+                return null;
+            }
+        }
     }
 }
